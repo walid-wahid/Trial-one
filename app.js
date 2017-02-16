3dd4220329660200a62e61b755f285a2afd22775
walid-wahid <marlinbrando1995@gmail.com>
2017-02-16 18:11:04 +0100

    this is the first commit i'll make

---
+++ app.js
@@ -0,0 +1,10 @@
+
+setTimeout(function() {
+console.log("3 seconds have aazazsas");
+},3000)
+var counter = function(arr){
+  return 'there are ' + arr.length + 'elements'
+} ;
+counter({'shaun' , 'crystal', 'ryu'}) ;
+var counter = require('./count') ;
+module.exports = counter ; 
